d6c79aaab96c8703640cc83e671b3ded1538091e
egavilan1987 <enmanuelgc@gmail.com>
2019-06-28 16:25:21 -0400

    Update help.php

--- help.php
+++ help.php
@@ -69,3 +69,12 @@ https://bootsnipp.com/snippets/BxA1B
     </div>
   </div>
 </div>
+
+        
+        -*******************************************************
+        Barcodes Generator Using PHP, MySQL and JsBarcode
+
+In the description talk about a little bit about what is barcode.
+- A short history of barcode and what it is.
+- Why it is important to use barcode.
+- Description of the program.

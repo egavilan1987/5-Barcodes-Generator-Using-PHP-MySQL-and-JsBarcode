30562a40dd20dbc6b777750adf200e9858c9b08c
egavilan1987 <enmanuelgc@gmail.com>
2019-06-28 14:56:27 -0400

    Update help.php

--- help.php
+++ help.php
@@ -1,1 +1,71 @@
 Description of the Application
+
+
+https://bootsnipp.com/snippets/BxA1B
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<h2>Barcodes Generator</h2>
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#barcodeModal">New Barcode</button>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-sm">
+                          <thead>
+                            <tr>
+                              <th scope="col">#</th>
+                              <th scope="col">Prodcut Name</th>
+                              <th scope="col">Barcode</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th scope="row">1</th>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">2</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                            </tr>
+                            <tr>
+                              <th scope="row">3</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="modal fade" id="barcodeModal" role="dialog" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New barcode</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Product name:</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Generate barcode</button>
+      </div>
+    </div>
+  </div>
+</div>
